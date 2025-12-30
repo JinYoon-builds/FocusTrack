0fb40e3666759fb5c9e06a9f018e5dfd57becf64
Jin.Yoon <jin.yoon.builds@gmail.com>
2025-12-30 13:10:40 +0900

    feat: implement multi-pose calibration for robust focus detection (#11)
    - 3단계 캘리브레이션 UX 구현 (정자세, 몰입, 이완 자세 측정)
    - 데이터 병합(Aggregation) 로직 추가: 다양한 자세 데이터를 통합하여 공분산 행렬 계산
    - 모델의 Robustness 강화: 데이터 분포(Variance)를 넓혀 자연스러운 움직임(기대기, 숙이기)에 대한 오탐지 방지

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from core.pose_wrapper import PoseWrapper
 from ui.renderer import Renderer
 from core.database import FocusLogger
 
-# 상태 상수
+# 상태 머신 STATE MACHINE
 STATE_WAITING = 0      # 준비
 STATE_CALIBRATING = 1  # 측정 중
 STATE_MONITORING = 2   # 감시 중
@@ -24,7 +24,11 @@ def main():
     # 캘리브레이터, 판정기 등 불러오기
     pose_wrapper = PoseWrapper()
     renderer = Renderer()
-    calibrator = PoseCalibrator(buffer_size=30)
+
+    FRAMES_PER_STEP = 30
+    TOTAL_STEPS = 3
+    calibrator = PoseCalibrator(buffer_size=FRAMES_PER_STEP * TOTAL_STEPS)
+
     analyzer = FocusAnalyzer(threshold=3.0)
     logger = FocusLogger(buffer_size=10)
 
@@ -32,6 +36,8 @@ def main():
     current_state = STATE_WAITING
     last_log_time = 0
     print("FocusTrack v2.0 시동... 캘리브레이션을 시작하려면 c를 누르세요.")
+    calib_step = 1 # 1: 정자세, 2: 몰입(앞으로 기댐), 3: 이완(뒤로 기댐)
+    is_measuring = False
 
     try:
         # 메인 루프
@@ -50,7 +56,7 @@ def main():
                 # 이번 프레임은 건너뛰고 다시 시도
                 continue
 
-            # 처리: 빼대 찾기
+            # 처리: 뼈대 찾기
             results = pose_wrapper.process(frame)
 
             # 로직 & 출력
@@ -64,18 +70,43 @@ def main():
 
             # 2. 캘리브레이션 화면
             elif current_state == STATE_CALIBRATING:
-                count = calibrator.add_data(results.pose_landmarks)
+                # 진행 상황
+                current_count = len(calibrator.buffer)
+                target_count = calib_step * FRAMES_PER_STEP
 
-                renderer.draw_calibration(frame, count)
+                # 측정 
+                if is_measuring:
+                    calibrator.add_data(results.pose_landmarks)
+                    message = f"Step {calib_step}: Measuring..."
 
-                # 데이터가 다 모이면 분석 모드로 전환
-                if calibrator.is_ready():
-                    print("✅ 캘리브레이션 완료! 분석 모드로 전환...")
+                    # 해당 스텝의 목표 달성 체크
+                    if len(calibrator.buffer) >= target_count:
+                        is_measuring = False
+                        print(f"✅ Step {calib_step} 완료!")
 
-                    mean_vec, inv_cov = calibrator.get_statistics()
-                    analyzer.set_standard_pose(mean_vec, inv_cov)
+                        # 수집 완료
+                        if calib_step == TOTAL_STEPS:
+                            print("✅ 캘리브레이션 완료.")
+                            # 통계치 계산
+                            mean_vec, inv_cov = calibrator.get_statistics()
+                            analyzer.set_standard_pose(mean_vec, inv_cov)
+                            current_state = STATE_MONITORING
 
-                    current_state = STATE_MONITORING
+                        else:
+                            calib_step += 1
+
+
+                # 대기
+                else:  
+                    if calib_step == 1:
+                        message = "Step 1: Sit straight and Press 'c'"
+                    elif calib_step == 2:
+                        message = "Step 2: Lean forward and Press 'c'"
+                    elif calib_step == 3:
+                        message = "Step 3: Lean Back and Press 'c'"
+                
+                renderer.draw_calibration(frame, message, current_count, target_count)
+
 
             # 3. 분석 모드 화면 + logging
             # ‼️ 코어 로직 ‼️
@@ -101,9 +132,16 @@ def main():
             if key == ord('q'):
                 print("프로그램을 종료합니다.")
                 break
-            elif key == ord('c') and current_state == STATE_WAITING:
-                print("사용자 캘리브레이션(최적화) 중...")
-                current_state = STATE_CALIBRATING
+            elif key == ord('c'):
+                # 처음 시작 시...
+                if current_state == STATE_WAITING:
+                    current_state = STATE_CALIBRATING
+                    calib_step = 1
+                    is_measuring = False
+                # 단계별 측정 시작
+                elif current_state == STATE_CALIBRATING and not is_measuring:
+                    is_measuring = True
+                    print(f"✅ Step {calib_step} 측정 시작...")
 
     finally:
         print("시스템 종료...")
--- src/ui/renderer.py
+++ src/ui/renderer.py
@@ -4,6 +4,7 @@ import mediapipe as mp
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+YELLOW = (0, 255, 255)
 
 class Renderer:
     def __init__(self):
@@ -18,17 +19,26 @@ class Renderer:
             )
 
     # 내부 헬퍼 함수
-    def _draw_text(self, frame, text, color):
-        cv2.putText(frame, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+    def _draw_text(self, frame, text, pos, color, scale = 1.0):
+        cv2.putText(frame, text, pos, cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
 
     # 상황별 화면 
     # 1. 대기 화면
     def draw_waiting(self, frame):
-        self._draw_text(frame, "Press 'c' to Calibrate", RED)
+        center_x = frame.shape[1] // 2
+        self._draw_text(frame, "Press 'c' to Calibrate", (center_x - 200, 100), RED, 1.2)
     
-    # 2. 캘리브레이션 화면: int count 필요
-    def draw_calibration(self, frame, count: int):
-        self._draw_text(frame, f"Collecting data ... {count} / 30", BLUE)
+    # 2. 캘리브레이션 화면: int count, total 필요, str 상태메시지 필요
+    def draw_calibration(self, frame, message: str, count: int, target:int):
+        center_x = frame.shape[1] // 2
+        
+        # 안내 문구 출력
+        self._draw_text(frame, message, (center_x - 200, 100), YELLOW, 1.0)
+
+        # 진행상황 출력
+        status = f"Progress: {count} / {target}"
+        self._draw_text(frame, status, (center_x - 150, 200), BLUE, 0.8)
+        
 
     # 3. 감시 화면: str status, float dist 필요
     def draw_monitoring(self, frame, status : str, dist : float):
